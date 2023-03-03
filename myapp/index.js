3e7bdf5a386f96c73f2d6195c052ba1ad655c268
signomarco <signomarco2003@gmail.com>
2023-03-03 15:08:50 +0100

    lupaGay

--- myapp/index.js
+++ myapp/index.js
@@ -1,5 +1,12 @@
 const http = require('http');
 const express = require('express');
-const
+const qs = require('querystring');
 
 const app = express();
+
+const HOSTNAME = '127.0.0.1';
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.set('Content-Type', 'application/json');
+});
